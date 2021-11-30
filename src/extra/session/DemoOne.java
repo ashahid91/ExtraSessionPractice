5ec942ef7a035ebbe78243e5b377d4ecfdc1be55
agg72 <agg72@DESKTOP-BN6NDO2>
2021-11-30 18:52:17 -0500

    added getName method

--- src/extra/session/DemoOne.java
+++ src/extra/session/DemoOne.java
@@ -4,6 +4,8 @@ public class DemoOne {
 	public static void main(String[] args) {
 		System.out.println("Let the fun begin");
 		System.out.println("I am ready ;)");
+		
+		getName("Amanda");
 	}
 
 	public static void OscartestingOne() {
@@ -12,5 +14,9 @@ public class DemoOne {
 	public static void getName(String name) {
 		
 		System.out.println("Tahmina");
+	}
+	
+	public static void getName(String name) {
+		System.out.println(name);
 	}
 }
