9712f308357afca84a2dd816a7e1b48ab5640a7f
Atorres87 <94851412+Atorres87@users.noreply.github.com>
2021-11-30 18:56:43 -0500

    change get name

--- src/extra/session/DemoOne.java
+++ src/extra/session/DemoOne.java
@@ -23,7 +23,7 @@ public class DemoOne {
 		System.out.println("Tahmina");
 	}
 	
-	public static void getName(String name) {
+	public static void getName1(String name) {
 		System.out.println(name);
 	}
 }
