a5c696e8509544128447948a5377122b7adccd24
Cheryl <Cheryl@LAPTOP-TI3OQHP8.mynetworksettings.com>
2021-11-30 21:18:50 -0500

    added new Demo Class

--- src/extra/session/ClassCheryl.java
+++ src/extra/session/ClassCheryl.java
@@ -9,21 +9,23 @@ public class ClassCheryl {
 	}
 
 	public static void overloading(int a, int b) {
-
+       
+        
 		System.out.println(a + b);
 	}
 
 	public static void overloading(int a, int b, int c) {
-
+      
 		System.out.println(a + b + c);
 
 	}
 
 	public static void overloading(int a, String name) {
-		a = 10;
+		
 		System.out.println(a);
-		name = "Cheryl";
+		
 		System.out.println(name);
 	}
+	
 	
 }
