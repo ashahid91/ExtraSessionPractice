18a817698df40e08c493ee98ee57c3552c7d5d7a
agg72 <agg72@DESKTOP-BN6NDO2>
2021-11-30 20:51:22 -0500

    added divNum()

--- src/extra/session/DemoTwo.java
+++ src/extra/session/DemoTwo.java
@@ -4,5 +4,12 @@ public class DemoTwo {
 
 	public static void main(String[] args) {
 		System.out.println("Hi from second class");
+		
+		divNum(60,2);
+	}
+	
+	
+	public static void divNum(int a, int b) {
+		System.out.println(a/b);
 	}
 }
