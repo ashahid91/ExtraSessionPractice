ce47d2618eef440d5150212d79a8bae04c8a609a
tahminzikrullah <tahminzikrullah@tahminas-mbp.fios-router.home>
2021-11-30 19:20:44 -0500

    added main()

--- src/extra/session/DemoOne.java
+++ src/extra/session/DemoOne.java
@@ -16,7 +16,6 @@ public class DemoOne {
 	
 	public static void CherylMethod() {
 		System.out.println("let's start the conflicts");
-		System.out.println("conflict was started from me :(");
 	}
 	public static int myMethod(int a, int b) {
 		int result = 0;
@@ -30,6 +29,32 @@ public class DemoOne {
 	
 
 
+
+
+		
+	public static void empId(int id) {
+	System.out.println("Minky's id commit..");
+
+	}
+	
+
+
+	public static void team(String name) {
+		System.out.println("Fieruz");
+	}
+
+
+	
+	public static void shahid() {
+
+		System.out.println("random");
+
+
+		System.out.println("random");
+
+	}
+
+	
 	public static void OscartestingOne() {
 		System.out.println("Attempting to create a conflict");
 		
@@ -54,5 +79,10 @@ public class DemoOne {
 	
 	public static void addNum(int a, int b) {
 		System.out.println(a+b);
+	}
+	
+	public static void main(String count) {
+		
+		count = "1,2,3...";
 	}
 }
