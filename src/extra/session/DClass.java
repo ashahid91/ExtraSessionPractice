147a2a0e134f78d1066595c093a92ff7b0c970e3
Diana-Banana <DianaStrezhesku@gmail.com>
2021-11-30 21:21:06 -0500

    diana commit

---
+++ src/extra/session/DClass.java
@@ -0,0 +1,25 @@
+package extra.session;
+
+public class DClass {
+	
+	public static void main(String[] args) {
+		 
+		getName ("Diana");
+		getAge (28);
+		getGender ('F');
+		getHobbie ("Cars");
+	}
+ 
+	public static void getName(String name) {
+		System.out.println(name);
+	}
+	public static void getAge(int age) {
+		System.out.println(age);
+	}
+	public static void getGender(char gender) {
+		System.out.println(gender);
+	}
+	public static void getHobbie(String hobbies) {
+		System.out.println(hobbies);
+	}
+}
