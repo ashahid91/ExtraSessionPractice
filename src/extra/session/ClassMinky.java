db57420ae4a27bd2dcf5d88fe6662b68a702665d
mink0 <mink0@DESKTOP-49NJAGQ.fios-router.home>
2021-11-30 21:16:53 -0500

    created new class

---
+++ src/extra/session/ClassMinky.java
@@ -0,0 +1,15 @@
+package extra.session;
+
+public class ClassMinky {
+
+	public static void main(String[] args) {
+		getId(777);
+
+	}
+
+	    public static void getId(int id) {
+	   System.out.println("Minky's conflicting ID: "+ id);
+	
+
+}
+}
