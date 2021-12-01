27b26fa0685baad535baffa3c4ac4d62397ff2fe
Wahida <Wahida@Wahida-pc.fios-router.home>
2021-12-01 06:03:06 +0430

    commit

--- src/extra/session/DemoOne.java
+++ src/extra/session/DemoOne.java
@@ -5,16 +5,12 @@ public class DemoOne {
 		System.out.println("Let the fun begin");
 		System.out.println("I am ready ;)");
 
-
 		String name = "waheeda";
 
 		getName("Amanda");
 
-		addNum(20,30);
-
-
+		addNum(20, 30);
 	}
-
 	public static void CherylMethod1() {
 		System.out.println("let's start the conflicts");
 
@@ -22,11 +18,7 @@ public class DemoOne {
 
 		addNum(20, 30);
 		nameOne("Cheryl");
-
 	}
-
-
-
 	public static void addName(int sajia) {
 		System.out.println(sajia);
 	}
@@ -34,12 +26,10 @@ public class DemoOne {
 	public static void CherylMethod() {
 		System.out.println("let's start the conflicts");
 
-
 		System.out.println("conflict was started from me :(");
 		System.out.println("pushing");
 
 		System.out.println("practicing conflicts");
-
 	}
 
 	public static String nameOne(String name) {
@@ -57,24 +47,19 @@ public class DemoOne {
 		sum = a + a + b + b;
 
 		return result;
-
 	}
-
 
 	public static void getMyname(String name) {
 		System.out.println("My name is: " + name);
 		System.out.println("Pull");
-
 	}
 
 	public static void empId(int id) {
 		System.out.println("Minky's id commit..");
-
 	}
 
 	public static void team(String name) {
 		System.out.println("Fieruz");
-
 	}
 
 	public static void shahid() {
@@ -82,20 +67,16 @@ public class DemoOne {
 		System.out.println("random");
 
 		System.out.println("random");
-
 	}
-
 
 	public static void OscartestingOne() {
 		System.out.println("Attempting to create a conflict");
 
 		System.out.println("Shada");
 
-	    System.out.println("Adding Shada2");
-
-		System.out.println("Adding Shada2");
 		System.out.println("Adding Shada2");
 
+		System.out.println("Adding Shada2");
 	}
 
 	public static void getName(String name) {
@@ -105,39 +86,31 @@ public class DemoOne {
 		System.out.println("this is my third try");
 	}
 
-
 	public static void getName2(String name) {
 		System.out.println(name);
 		// my name is sajia
 	}
 
 	public static void OscartestingTwo() {
-		System.out.println("I feel lucky right now");
+		System.out.println("I feel lucky right now");// keep it up
 	}
 
 	public static void addNum(int a, int b) {
 		System.out.println(a + b);
 	}
 
-
-
-
-
 	public static void main(String count) {
 
 		count = "1,2,3...";
-
 	}
 
 	public static void getInfo(String lastName) {
 		System.out.println("Atiq");
 
-
 	}
 
 	public static void subtractThreeNumbers(int numOne, int numTwo, int numThree) {
-		System.out.println(numOne - numTwo - numThree); //Diana
+		System.out.println(numOne - numTwo - numThree); // Diana
 
 	}
-
 }
